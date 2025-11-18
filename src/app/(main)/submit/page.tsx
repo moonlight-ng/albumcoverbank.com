7a3eaab376a4351365a73d76a33bc87d12ec6161
Taqib <taqib.ibrahim@gmail.com>
2025-11-18 20:49:51 +0100

    chore: update submit link

--- src/app/(main)/submit/page.tsx
+++ src/app/(main)/submit/page.tsx
@@ -47,7 +47,7 @@ export default function SubmitPage() {
 
           <div className="pt-4">
             <Button asChild className="w-full rounded-full" size="lg">
-              <Link href="mailto:ope@moonlight.ng?subject=New%20Cover">
+              <Link href="https://www.notion.so/moonlight-studios/2af599ecfca680b9a98eda781e2935b0">
                 Submit a cover
               </Link>
             </Button>
